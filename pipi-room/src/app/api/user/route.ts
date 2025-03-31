6ce1116f4d819de2f7ef9f6d1348e9e0cab1a8ef
石川立騎 <ritsuritsu143@gmail.com>
2025-03-31 21:24:26 +0900

    ユーザが取得できないバグの修正

--- pipi-room/src/app/api/user/route.ts
+++ pipi-room/src/app/api/user/route.ts
@@ -1,6 +1,10 @@
+import { NextResponse } from "next/server";
 import { db } from "@/db";
 
 export async function GET() {
-  const users = await db.query.users.findMany()
-  return Response.json(users)
+  const users = await db.query.users.findMany();
+  const response = NextResponse.json(users);
+  // キャッシュを無効にするためのヘッダー設定
+  response.headers.set("Cache-Control", "no-store, max-age=0");
+  return response;
 }
